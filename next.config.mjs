e83d1f1c6382614a977632efe2d0d83511ccd50a
Wilbrord <bwilbrord@gmail.com>
2025-06-13 14:50:51 +0200

    resolve image

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         hostname: "107.23.87.13",
       },
       {
+        protocol: "https",
+        hostname: "api.mahaliafrica.com",
+      },
+      {
         protocol: "http",
         hostname: "207.180.253.55",
         port: "8090",
